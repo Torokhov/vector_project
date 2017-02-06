c243e57d6d8c665b83352081403430dff951dc7c
Oleg Torokhov <torokhov093@gmail.com>
2017-02-06 18:37:58 +0700

    Add test for constuctor 'Vector'

---
+++ js/test.js
@@ -0,0 +1,7 @@
+describe("Vector", function() {
+  it("Создание объекта Vector", function(){
+    var vector = new Vector(1, 4);
+    assert.equal(vector.x, 1);
+    assert.equal(vector.x, 4);
+  });
+});
