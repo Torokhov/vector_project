772de2ac92c6245cceb31b561342cfb96ee2a53f
Oleg Torokhov <torokhov093@gmail.com>
2017-02-06 18:41:26 +0700

    correct test for constuctor 'Vector'

--- js/test.js
+++ js/test.js
@@ -2,6 +2,6 @@ describe("Vector", function() {
   it("Создание объекта Vector", function(){
     var vector = new Vector(1, 4);
     assert.equal(vector.x, 1);
-    assert.equal(vector.x, 4);
+    assert.equal(vector.y, 4);
   });
 });
